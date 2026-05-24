b5ca3716f915218afeffe730448f0052a58fe9c3
Alex Chorvadi <chorvadikrushi01@gmail.com>
2026-05-24 20:33:39 +0530

    added test msg

--- 11 - Password Reset/backend/config/db.js
+++ 11 - Password Reset/backend/config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully test");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
