31f0312a1f46e5c21461ac011c766a85fdf4dbad
Alex Chorvadi <chorvadikrushi01@gmail.com>
2026-05-27 23:27:36 +0530

    try to fix transporter

--- 11 - Password Reset/backend/config/mail_transporter.js
+++ 11 - Password Reset/backend/config/mail_transporter.js
@@ -3,32 +3,22 @@ const nodemailer = require("nodemailer");
 console.log("BEFORE TRANSPORT");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 587,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
-
-  family: 4,
-
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
 });
 
-console.log("AFTER TRANSPORT");
-
-transporter.verify((error, success) => {
-
-  console.log("VERIFY CALLBACK");
-
-  if (error) {
-    console.log("VERIFY ERROR:", error);
-  } else {
-    console.log("SMTP READY");
-  }
-
-});
+try {
+  await transporter.verify();
+  console.log("Server is ready to take our messages");
+} catch (err) {
+  console.error("Verification failed:", err);
+}
 
 module.exports = transporter;
