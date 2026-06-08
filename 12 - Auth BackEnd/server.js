d0d10ba7190313e30c8c8cc1ad4e8af9a17bdc56
Alex Chorvadi <chorvadikrushi01@gmail.com>
2026-06-09 00:14:17 +0530

    added note

--- 12 - Auth BackEnd/server.js
+++ 12 - Auth BackEnd/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Recipes API");
+    res.send("Please Login Or Register to access the API");
 });
 
 app.use("/api", authRoutes);
